58fb4c534c2af46ef4c98706c0918fc6cb341cb3
MasakiPrivate <masaki_twins@yahoo.co.jp>
2023-08-22 23:19:43 +0900

    no message

--- Assets/Scripts/System/CsvImporter.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "MyScriptable/Create CSV Impoter")]
-public class CsvImporter : ScriptableObject {
-	
-}
