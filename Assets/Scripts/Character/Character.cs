43055725c46b9da2a21ea511f04c44cbaf1ca6fa
MasakiPrivate <masaki_twins@yahoo.co.jp>
2023-08-25 08:52:19 +0900

    [Update] FIx Update

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -6,27 +6,47 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // ---------- 定数宣言 ----------
+    // ---------- ゲームオブジェクト参照変数宣言 ----------
+    // ---------- プレハブ ----------
+    [SerializeField, Tooltip("キャラクターデータ")] private CharacterData _characterData = default;
+    [SerializeField, Tooltip("画像上書き")] private OverrideSprite _overrideSprite = default;
+    // ---------- プロパティ ----------
+    // ---------- クラス変数宣言 ----------
+    // ---------- インスタンス変数宣言 ----------
+    // ---------- Unity組込関数 ----------
     void Start()
     {
-        
+        SetUp(_characterData, true, -1);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
-
+    // ---------- Public関数 ----------
     //　セットアップ
-    public Character SetUp(int characterId, bool isEnemy ,int pos_z = -1)
+    public Character SetUp(CharacterData characterData, bool isAlly ,int pos_z = -1)
     {
         // 奥行に多少のばらつきを持たせる
         if(pos_z == -1)
             pos_z = Random.Range(0, 10);
 
+        // データチェック
+        if(characterData != null)
+            _characterData = characterData;
+        if(_characterData == null)
+        {
+            Debug.Log("キャラクターデータがありません");
+            return null;
+        }
+
+        _overrideSprite.overrideTexture = _characterData.texture;
+
         // データベースからパラメータを読み込んで反映させる
 
         return this;
     }
+    // ---------- Private関数 ----------
+    // ---------- protected関数 ----------
 }
--- Assets/Scripts/Character/GameMainManager.cs
+++ Assets/Scripts/Character/GameMainManager.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InGameManager : MonoBehaviour
+public class GameMainManager : MonoBehaviour
 {
     // ---------- 定数宣言 ----------
     // ---------- ゲームオブジェクト参照変数宣言 ----------
     // ---------- プレハブ ----------
     // ---------- プロパティ ----------
+    [SerializeField, Tooltip("インゲームマネージャー")] private InGameManager _inGameManager = default;
     // ---------- クラス変数宣言 ----------
     // ---------- インスタンス変数宣言 ----------
     // ---------- Unity組込関数 ----------
